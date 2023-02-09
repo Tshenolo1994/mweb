fefe81314108151a04b42c639be809b9970f6c59
Tshenolo Lobelo <63031932+Tshenolo1994@users.noreply.github.com>
2023-02-09 04:26:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-// 1. import `ChakraProvider` component
-import { ChakraProvider , Box, CSSReset} from '@chakra-ui/react'
+import { ChakraProvider , Box} from '@chakra-ui/react'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import { Products } from './components/Products'
